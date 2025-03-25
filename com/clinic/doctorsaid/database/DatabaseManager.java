b112024ee80b4d2a70c556935fa1d3e9f6b2e6fa
james <james.anthony.russell36.com>
2025-03-25 18:40:05 +1100

    ui up and running and database connection added.

--- com/clinic/doctorsaid/database/DatabaseManager.java
+++ com/clinic/doctorsaid/database/DatabaseManager.java
@@ -1,10 +1,47 @@
 package com.clinic.doctorsaid.database;
 
-public class DatabaseManager {
+import java.sql.*;
 
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.PreparedStatement;
-	import java.sql.SQLException;
-	
-}
+import com.clinic.doctorsaid.model.Patient;
+
+public class DatabaseManager {
+    private Connection connect() {
+        String url = "jdbc:sqlite:doctorsaid.db";
+        try {
+            return DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+// initialize database if it doesnt exist 
+
+    public void initializeDatabase() {
+    	String sql = "CREATE TABLE IF NOT EXISTS patients (" +
+                 "id INTEGER PRIMARY KEY, " +
+                 "name TEXT NOT NULL, " +
+                 "medical_history TEXT)";
+    	try (Connection conn = this.connect();
+    			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+    			pstmt.executeUpdate();
+    			System.out.println("Patients table created or already exists.");
+    	} catch (SQLException e) {
+    		System.out.println(e.getMessage());
+    	}
+    }
+    
+    public void addPatient(Patient patient) {
+		String sql = "INSERT INTO patients(id, name, medical_history) VALUES(?,?,?)";
+		try (Connection conn = this.connect();
+			  PreparedStatement pstmp = conn.prepareStatement(sql)) {
+			pstmp.setInt(1, patient.getID());
+			pstmp.setString(2, patient.getName());
+			pstmp.setString(3, patient.getMedicalHistory());
+			pstmp.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+    }
+}	
